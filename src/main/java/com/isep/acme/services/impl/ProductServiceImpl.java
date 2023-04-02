8c7878ed3ce116ad5d05657987149b182cfa3f68
João Rocha <1220256@isep.ipp.pt>
2023-04-03 00:04:26 +0100

    Created Rabbit Consumer

--- src/main/java/com/isep/acme/services/impl/ProductServiceImpl.java
+++ src/main/java/com/isep/acme/services/impl/ProductServiceImpl.java
@@ -1,10 +1,11 @@
 package com.isep.acme.services.impl;
 
+import com.isep.acme.rabbit.RMQConfig;
 import com.isep.acme.model.Product;
 import com.isep.acme.model.dtos.ProductDTO;
 import com.isep.acme.repositories.ProductRepository;
-
 import com.isep.acme.services.ProductService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +17,15 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public ProductDTO create(final Product product) {
         final Product p = new Product(product.getSku(), product.getDesignation(), product.getDescription());
-
+        rabbitTemplate.convertAndSend(RMQConfig.EXCHANGE, "", p);
+        System.out.println("Product" + p);
+        System.out.println("Product SKU" + product.getSku());
         return repository.save(p).toDto();
     }
 
