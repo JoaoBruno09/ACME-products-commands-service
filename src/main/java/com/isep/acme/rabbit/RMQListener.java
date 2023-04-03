b2e18a9e25d9982e7b5c100d37d417773a91fe19
João Rocha <1220256@isep.ipp.pt>
2023-04-04 00:10:15 +0100

    Added Rabbit Listener

---
+++ src/main/java/com/isep/acme/rabbit/RMQListener.java
@@ -0,0 +1,15 @@
+package com.isep.acme.rabbit;
+
+import com.isep.acme.model.Product;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RMQListener {
+
+    @RabbitListener(queues = RMQConfig.PCQUEUE)
+    public void listener(Product product){
+        System.out.println("Product" + product);
+        System.out.println("Product SKU" + product.getSku());
+    }
+}
