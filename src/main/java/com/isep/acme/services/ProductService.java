01b7a3bc83d5fe98b03c6bc179d82fccb1fc680a
João Rocha <1220256@isep.ipp.pt>
2023-04-08 22:31:58 +0100

    create, update and delete product is publishing a message to Rabbit and changed the way how 2 instances are created.

--- src/main/java/com/isep/acme/services/ProductService.java
+++ src/main/java/com/isep/acme/services/ProductService.java
@@ -2,6 +2,7 @@ package com.isep.acme.services;
 
 import com.isep.acme.model.Product;
 import com.isep.acme.model.dtos.ProductDTO;
+import org.springframework.amqp.core.MessagePostProcessor;
 
 public interface ProductService {
 
@@ -10,4 +11,6 @@ public interface ProductService {
     ProductDTO updateBySku(final String sku, final Product product);
 
     void deleteBySku(final String sku);
+
+    MessagePostProcessor createMessageProcessor(String header);
 }
--- src/main/java/com/isep/acme/services/impl/ProductServiceImpl.java
+++ src/main/java/com/isep/acme/services/impl/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.isep.acme.model.Product;
 import com.isep.acme.model.dtos.ProductDTO;
 import com.isep.acme.repositories.ProductRepository;
 import com.isep.acme.services.ProductService;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,10 @@ import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private static String createdProductHeader = "product-created";
+    private static String updatedProductHeader = "product-updated";
+    private static String deletedProductHeader = "product-deleted";
 
     @Autowired
     private ProductRepository repository;
@@ -23,28 +28,36 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDTO create(final Product product) {
         final Product p = new Product(product.getSku(), product.getDesignation(), product.getDescription());
-        rabbitTemplate.convertAndSend(RMQConfig.EXCHANGE, "", p);
-        System.out.println("Product" + p);
-        System.out.println("Product SKU" + product.getSku());
+        rabbitTemplate.convertAndSend(RMQConfig.EXCHANGE, "", p, createMessageProcessor(createdProductHeader));
         return repository.save(p).toDto();
     }
 
     @Override
     public ProductDTO updateBySku(String sku, Product product) {
-        
         final Optional<Product> productToUpdate = repository.findBySku(sku);
-
-        if( productToUpdate.isEmpty() ) return null;
-
-        productToUpdate.get().updateProduct(product);
-
-        Product productUpdated = repository.save(productToUpdate.get());
-        
-        return productUpdated.toDto();
+        if(!productToUpdate.isEmpty()){
+            productToUpdate.get().updateProduct(product);
+            Product productUpdated = repository.save(productToUpdate.get());
+            rabbitTemplate.convertAndSend(RMQConfig.EXCHANGE, "", productUpdated, createMessageProcessor(updatedProductHeader));
+            return productUpdated.toDto();
+        }
+        return null;
     }
 
     @Override
     public void deleteBySku(String sku) {
-        repository.deleteBySku(sku);
+        final Optional<Product> productToDelete = repository.findBySku(sku);
+        if(!productToDelete.isEmpty()){
+            rabbitTemplate.convertAndSend(RMQConfig.EXCHANGE, "", productToDelete.get(), createMessageProcessor(deletedProductHeader));
+            repository.deleteBySku(sku);
+        }
+    }
+
+    @Override
+    public MessagePostProcessor createMessageProcessor(String header) {
+        return message -> {
+            message.getMessageProperties().setHeader("action", header);
+                return message;
+        };
     }
 }
